6b664e3f8a265922234a8a4d5d2bf2ff7c39da91
amankrsah <69616665+amankrsah@users.noreply.github.com>
2022-06-15 16:19:24 +0530

    First commit

---
+++ components/navbar.php
@@ -0,0 +1,11 @@
+<nav>
+    <ul>
+        <img src="assets/web-bg.svg" alt="" srcset="">
+        <li id="home">Home</li>
+        <li>About</li>
+        <li>Contact us</li>
+        <li>Login</li>
+        <li id="login">Register</li>
+    </ul>
+</nav>
+    
---
+++ index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
+    <title>Document</title>
+</head>
+<body>
+    <?php include("components/navbar.php") ?>
+    
+    <!-- <div class="login">
+        <span style="font-size: 50px;margin:">Login</span>
+        <input type="text" placeholder="Enter User name"><br>
+        <input placeholder="Enter Password" type="password">
+        <input class="btn" id="loginBtn"  placeholder="Login" type="submit">
+
+    </div> -->
+    <div id="loginForm">
+        <center>
+            
+            <div style="margin-bottom: -10px;margin-top:80px;padding: 25px">Login</div>
+            <input placeholder="USERNAME">
+            <input type="password" placeholder="PASSWORD">
+            <div id="btn" style="margin-top:10px">Login</div>
+            <div style="font-size: 20px;margin-top:10px"">or</p>
+            <div id="btn">Register</div>
+        </center>
+    </div>
+</body>
+</html>
