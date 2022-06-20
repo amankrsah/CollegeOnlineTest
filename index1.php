efaacfd5058e774fa5545f87e1f18f6bf45fd386
Anurag3012 <100462923+Anurag3012@users.noreply.github.com>
2022-06-20 13:18:51 +0530

    Create index1.php
    home page h ye 
    jisme login or registration ka option h

---
+++ index1.php
@@ -0,0 +1,37 @@
+<!-- -------------------- Home page (Anurag)--------------------------------------------------- -->
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Ubuntu:wght@300&display=swap" rel="stylesheet">
+    <title>Columbia</title>
+</head>
+
+<body>
+    <header>
+        <?php include("../Anurag/components/navbar.php") ?>
+        <div class="main">
+            <div class="img">
+                <img src="columbia.jpg" alt="loading...">
+            </div>
+            <h1>Columbia</h1>
+        </div>
+        <div class="para">
+            <h1 >Online test</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab suscipit rerum, distinctio, voluptas odio temporibus dolorum ea velit error quo ut pariatur, nostrum sint deserunt voluptatem asperiores deleniti voluptatum! Dicta voluptatum exercitationem quaerat quae deserunt eaque omnis fuga reiciendis ipsum. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Neque ipsa hic quibusdam ab distinctio voluptatum exercitationem aliquid quo tempore dolorem autem est molestias, quod at obcaecati provident, doloribus iure.
+            </p>
+            <div class="button">
+                <a href="#" class="btn">Login</a>
+                <a href="#" class="btn">Registor</a>
+            </div>
+        </div>
+    </header>
+</body>
+
+</html>
