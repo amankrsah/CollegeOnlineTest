fb01c57edce8cfd2c7bb9c44ef3333d1014370cb
Aman <amankrsah97555@gmail.com>
2022-07-05 11:08:41 +0530

    Added Register

--- components/navbar.php
+++ components/navbar.php
@@ -1,12 +1,12 @@
 <nav>
     <ul>
-        <img src="assets/web-bg.svg" alt="" srcset="">
-        <li id="home">Home</li>
-        <li>About</li>
-        <li>Contact us</li>
-        <li>Login</li>
-        <li id="login">Register</li>
-        <li>Contact Us</li>
+        <img src="http://localhost/php_/College_project/assets/web-bg.svg" alt="" srcset="">
+        <li id="home" <a href="http://localhost/php_/College_project/">Home</a></li>
+        <li><a href="">About</a></li>
+        <li><a href="">Contact us</a></li>
+        <li><a href="http://localhost/php_/College_project/">Login</a></li>
+        <li id="login"><a href="http://localhost/php_/College_project/pages/register.php">Register</a></li>
+        <li><a href="">Contact Us</a></li>
     </ul>
 </nav>
     
