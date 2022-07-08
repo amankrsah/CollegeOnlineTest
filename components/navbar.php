2360f11c3f74dd4d868c8f9f9326a71548ffd9ae
Aman <amankrsah97555@gmail.com>
2022-07-08 15:13:19 +0530

    Added Exam.php ,logo added,background problem solved,color theme improved

--- components/navbar.php
+++ components/navbar.php
@@ -1,12 +1,12 @@
 <nav>
     <ul>
-        <img src="http://localhost/php_/College_project/assets/web-bg.svg" alt="" srcset="">
-        <li id="home" <a href="http://localhost/php_/College_project/">Home</a></li>
-        <li><a href="">About</a></li>
-        <li><a href="">Contact us</a></li>
-        <li><a href="http://localhost/php_/College_project/">Login</a></li>
-        <li id="login"><a href="http://localhost/php_/College_project/pages/register.php">Register</a></li>
-        <li><a href="">Contact Us</a></li>
+        <img src="http://localhost/php_/College_project/assets/logo.jpg" alt="Logo" srcset="">
+        <li id="home"><a href="http://localhost/php_/College_project/">HOME</a></li>
+        <li><a href="">ABOUT</a></li>
+        <li><a href="http://localhost/php_/College_project/">LOGIN</a></li>
+        <li><a href="http://localhost/php_/College_project/pages/exam.php">EXAM</a></li>
+        <li id="login"><a href="http://localhost/php_/College_project/pages/register.php">REGISTER</a></li>
+        <li><a href="">CONTACT US</a></li>
     </ul>
 </nav>
     
